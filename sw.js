d6b68b26db70ef5255e90fe82a92147c0d626a0f
Mavis <[email protected]>
2026-07-24 07:55:01 +0000

    todo PWA with daily industry brief

---
+++ sw.js
@@ -0,0 +1,48 @@
+// todo · Service Worker
+const CACHE_NAME = 'todo-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png',
+  '/pages/home.html',
+  '/pages/brief.html',
+  '/pages/add-task.html',
+  '/pages/all-tasks.html',
+  '/pages/settings.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(
+        (cached) =>
+          cached ||
+          fetch(event.request).then((response) => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+            return response;
+          })
+      )
+    );
+  }
+});
